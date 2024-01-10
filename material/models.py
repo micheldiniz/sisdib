c71389cedb6905bfcdce0e537932cb208e9711ef
michel diniz <micheldiniz@ibc.gov.br>
2024-01-10 16:11:29 -0300

    add material model classes

--- material/models.py
+++ material/models.py
@@ -3,10 +3,28 @@ from django.db import models
 # Create your models here.
 
 class Material(models.Model):
-    pass
+    TIPO_CHOICES = {
+        "didático":"didático",
+        "paradidático":"paradidático",
+        "revista":"revista",
+        "DAL (S/Revisão)":"DAL (S/Revisão)",
+        "Outro":"Outro",
+    }
+    tipo = models.CharField(max_length = 255, choices=TIPO_CHOICES)
+    acervo = models.CharField(max_length = 255)
+    tiragem = models.CharField(max_length = 255)
+    quantidade_paginas = models.PositiveIntegerField()
+    arquivo_original = models.FileField(upload_to="arquivos/%Y/%m")
+    is_disponivel_para_assinatura = models.BooleanField(default=False)
+    is_disponivel_para_pedido = models.BooleanField(default=False)    
 
 class MaterialBraille(Material, models.Model):
-    pass
+    arquivo_braille = models.FileField(upload_to="arquivos/braille/%Y/%m")
+    quantidade_paginas_braille = models.PositiveIntegerField()
+    partes = models.PositiveIntegerField()
 
 class MaterialAmpliado(Material, models.Model):
-    pass
+    quantidade_paginas_ampliada = models.PositiveIntegerField()
+    arquivo_braille = models.FileField(upload_to="arquivos/ampliado/%Y/%m")
+    partes = models.PositiveIntegerField()    
+
