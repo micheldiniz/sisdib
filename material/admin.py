6ff12f04d850d96984bba682599f82828670b735
michel diniz <micheldiniz@ibc.gov.br>
2024-01-11 15:43:44 -0300

    creation of admin form

--- material/admin.py
+++ material/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from material.models import Material, MaterialAmpliado, MaterialBraille
 
 # Register your models here.
+
+class MaterialAdmin(admin.ModelAdmin):
+    pass
+
+class MaterialAmpliadoAdmin(admin.ModelAdmin):
+    pass
+
+class MaterialBrailleAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(MaterialBraille, MaterialBrailleAdmin)
+admin.site.register(MaterialAmpliado, MaterialAmpliadoAdmin)
+admin.site.register(Material, MaterialAdmin)
+
+
