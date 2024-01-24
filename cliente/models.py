c0c9f07dcb6496f644898778e750f3c4340d2313
michel <michel_dinizdasilva@yahoo.com.br>
2024-01-24 18:25:17 -0300

    change ESTADO_CHOICES remove unnecessary option

--- cliente/models.py
+++ cliente/models.py
@@ -8,11 +8,11 @@ from pessoa.models import Pessoa
 
 class Assinatura(models.Model):
     ESTADO_CHOICES =[
-        ('0','nenhuma'),
-        ('1','cancelado'),
-        ('2','vigente'),
+        # ('0','nenhuma'),
+        ('1','vigente'),
+        ('0','cancelado'),
     ]
-    estado = models.CharField(max_length=2,choices=ESTADO_CHOICES)
+    estado = models.CharField(max_length=2,choices=ESTADO_CHOICES, default='vigente')
     material = models.ForeignKey(Material, on_delete=models.CASCADE, null=True)
 
 class Cliente(models.Model):
