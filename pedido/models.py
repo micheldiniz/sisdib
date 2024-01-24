1a2c6712bc7332df6e9e5fb836ff4ca90413abef
michel <michel_dinizdasilva@yahoo.com.br>
2024-01-24 18:23:05 -0300

    add generic foreign key for material in Item

--- pedido/models.py
+++ pedido/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from material.models import Material
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 # Create your models here.
 class Item(models.Model):
@@ -9,7 +11,16 @@ class Item(models.Model):
         "pendente":"pendente",
     }    
     quantidade = models.PositiveIntegerField()
-    material = models.ForeignKey(Material, on_delete=models.CASCADE)
+    # material = models.Gener(Material, on_delete=models.CASCADE)
+    material_object_id = models.IntegerField()
+    material_content_type = models.ForeignKey(
+        ContentType,
+        on_delete = models.PROTECT,
+    )
+    material = GenericForeignKey(
+        'material_content_type',
+        'material_object_id',
+    )
     estado = models.CharField(max_length=255, choices=ESTADO_CHOICES)
     observacao = models.CharField(max_length=255)
     
