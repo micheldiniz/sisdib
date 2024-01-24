fb74bff445537d0459bf0996672e8dc62f69e774
michel <michel_dinizdasilva@yahoo.com.br>
2024-01-24 18:24:23 -0300

    add admin.py for pedido

--- pedido/admin.py
+++ pedido/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from pedido.models import Pedido, Item
 
 # Register your models here.
+
+class PedidoAdmin(admin.ModelAdmin):
+    pass
+
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Pedido, PedidoAdmin)
+admin.site.register(Item, ItemAdmin)
