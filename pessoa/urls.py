d9d4be843255d83ecaf3e39f3df59171a3255754
michel <michel_dinizdasilva@yahoo.com.br>
2024-01-29 18:43:29 -0300

    add success page to app pessoa

--- pessoa/urls.py
+++ pessoa/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import success_page,pessoafisica_registration
 
 urlpatterns = [
-    # path("", views.index, name="index")
+    path("cadastrar", pessoafisica_registration, name="pessoa_fisica_registration"),
+    path("success/", success_page, name="success_page"),
 ]
