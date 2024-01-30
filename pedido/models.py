9acbb86a02d0922a4a454fab5647765c83b12e97
michel diniz <micheldiniz@ibc.gov.br>
2024-01-30 17:02:25 -0300

    change Item to ItemPedido and relationship

--- pedido/models.py
+++ pedido/models.py
@@ -4,12 +4,21 @@ from django.contrib.contenttypes.models import ContentType
 from cliente.models import Cliente
 
 # Create your models here.
-class Item(models.Model):
+
+class Pedido(models.Model):        
+    data_registro = models.DateTimeField(auto_now_add=True)
+    numero_pedido = models.PositiveBigIntegerField()
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+
+    def __str__(self):        
+        return self.numero_pedido.__str__()
+
+class ItemPedido(models.Model):
     ESTADO_CHOICES = {
         "aceito":"aceito",
         "indisponivel":"indisponivel",
         "pendente":"pendente",
-    }    
+    }
     quantidade = models.PositiveIntegerField()
     material_object_id = models.IntegerField()
     material_content_type = models.ForeignKey(
@@ -22,8 +31,13 @@ class Item(models.Model):
     )
     estado = models.CharField(max_length=255, choices=ESTADO_CHOICES)
     observacao = models.CharField(max_length=255)
-    
-class Pedido(models.Model):    
-    items = models.ManyToManyField(Item)
-    data_registro = models.DateTimeField(auto_now_add=True)
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)    
+
+    def getGenericObject(self):
+        content_type = ContentType.model_class(self.material_content_type)
+        obj = content_type.objects.get(id=self.material_object_id)
+        return obj
+
+    def __str__(self):  
+        obj =  self.getGenericObject() 
+        return f"{obj.__str__()} quantidade: {self.quantidade}"
