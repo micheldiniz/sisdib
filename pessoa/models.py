a538d1c3d4ff2de05c6149934e1a21589121d540
michel diniz <micheldiniz@ibc.gov.br>
2024-01-31 15:06:31 -0300

    gitignore .csv files

--- pessoa/models.py
+++ pessoa/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Pessoa(models.Model):
     nome =  models.CharField(max_length=255,null=True)    
     data_registro = models.DateField(auto_now_add=True)
+    
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
 
