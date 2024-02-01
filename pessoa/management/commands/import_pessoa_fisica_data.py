e46131c3e6f5f1c803cda77e6c71f5c97c5509ea
michel diniz <micheldiniz@ibc.gov.br>
2024-02-01 16:54:42 -0300

    pessoa_fisica import command

---
+++ pessoa/management/commands/import_pessoa_fisica_data.py
@@ -0,0 +1,49 @@
+from django.core.management.base import BaseCommand, CommandError
+from pessoa.models import PessoaFisica, Endereco, Contato
+from cliente.models import Cliente
+import csv, datetime
+
+class Command(BaseCommand):
+    help = 'Importa dados para pessoa física (CPF é definido como valor único, porém null é aceito)'
+
+    # def add_arguments(seld):
+    #     pass
+
+
+    def handle(self, *args, **options):
+        # Your data import logic here
+        with open('pessoa/management/data/pessoa_fisica.csv', 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            date = ""
+            for row in reader:
+                if(len(row['data_registro']) > 0):
+                    date = datetime.datetime.strptime(row['data_registro'],"%d/%m/%Y").strftime("%Y-%m-%d")
+
+                pessoa = PessoaFisica.objects.create(
+                    nome=row['nome'],
+                    data_registro=date
+                    # data=row['field2'],
+                    # Map CSV fields to model fields
+                )
+
+                # Create Endereco
+                endereco = Endereco.objects.create(
+                    pessoa=pessoa,  # OneToOne relationship
+                    # logradouro=row['logradouro'],
+                    # other fields
+                )
+
+                # Create Contato
+                contato = Contato.objects.create(
+                    pessoa=pessoa,  # OneToOne relationship
+                    # telefone=row['telefone'],
+                    # other fields
+                )
+
+                cliente = Cliente.objects.create(
+                    pessoa = pessoa
+                )
+
+
+
+        self.stdout.write(self.style.SUCCESS('Data imported successfully'))
