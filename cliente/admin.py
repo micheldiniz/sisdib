f1d01f7b3bdb361d2c017415606672ede1d7c05c
michel diniz <micheldiniz@ibc.gov.br>
2024-02-19 17:21:44 -0300

    hide field data_ultima_alteracao

--- cliente/admin.py
+++ cliente/admin.py
@@ -5,6 +5,7 @@ from cliente.models import *
 class AssinaturaAdmin(admin.ModelAdmin):
     raw_id_fields = ('material',)
     autocomplete_fields = ('material',) 
+    exclude = ('data_ultima_alteracao',)
 
 class ClienteAdmin(admin.ModelAdmin):
     pass
