7da4c2ca6e956fed624fa78ab24b064afd2fbe4b
michel diniz <micheldiniz@ibc.gov.br>
2024-02-22 17:42:38 -0300

    add inline for Cliente on assinatura

--- cliente/admin.py
+++ cliente/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 from cliente.models import *
 
+class AssinanteInline(admin.StackedInline):
+    model = Assinante    
+    extra = 0
 
 class AssinaturaAdmin(admin.ModelAdmin):
+    inlines = [AssinanteInline]
     raw_id_fields = ('material',)
-    autocomplete_fields = ('material',) 
+    autocomplete_fields = ('material','solicitante') 
     exclude = ('data_ultima_alteracao',)
 
+
 class ClienteAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['pessoa__nome','pessoa__nacionalidade', 'pessoa__endereco__pais']
 
 admin.site.register(Cliente, ClienteAdmin)
 admin.site.register(Assinatura, AssinaturaAdmin)
