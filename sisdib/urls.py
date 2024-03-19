3151083b3a666ddd965866216bee6214e86396dc
michel diniz <micheldiniz@ibc.gov.br>
2024-03-19 15:07:22 -0300

    add pattern for Pedido and Material apps

--- sisdib/urls.py
+++ sisdib/urls.py
@@ -16,9 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cliente/', include("cliente.urls")),    
     path('pessoa/', include("pessoa.urls")),
+    path('material/', include("material.urls")),
+    path('pedido/', include("pedido.urls")),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(template_name = 'login.html', redirect_authenticated_user = True),
+        name = 'login'
+    ),
+    path('logout/', auth_views.LogoutView.as_view(next_page = '/login/'), name = 'logout'),
 ]
