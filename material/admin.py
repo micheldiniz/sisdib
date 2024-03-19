1d200a01c2e0a8c94373c8d559320bbab9c3aed4
michel diniz <micheldiniz@ibc.gov.br>
2024-03-19 16:49:36 -0300

    Add list display and editable fields

--- material/admin.py
+++ material/admin.py
@@ -12,10 +12,19 @@ class MaterialAdaptadoInline(admin.StackedInline):
 
 class MaterialAdmin(admin.ModelAdmin):
     inlines = [MaterialAdaptadoInline]
-    verbose_name_plural = 'Materiais'
+    search_fields = ['titulo','classificacao']
 
-class MaterialAdaptadoAdmin(admin.ModelAdmin):
+
+class MaterialAdaptadoAdmin(admin.ModelAdmin):   
     search_fields = ['material__titulo','tipo']
+    list_display = ['material_titulo','tipo','is_disponivel_para_pedido','is_disponivel_para_assinatura']
+    list_editable = ['is_disponivel_para_pedido','is_disponivel_para_assinatura']
+    autocomplete_fields = ('material',)
+
+    def material_titulo(self, obj):
+        return obj.material.titulo if obj.material else None
+
+    material_titulo.short_description = 'Título do material'
 
 admin.site.register(Material, MaterialAdmin)
 admin.site.register(MaterialAdaptado, MaterialAdaptadoAdmin)
