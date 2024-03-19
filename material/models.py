c60d8df145b855f820f219674a2951ec8c3dddbb
michel diniz <micheldiniz@ibc.gov.br>
2024-03-19 15:18:04 -0300

    fix CHOICES to iterable

--- material/models.py
+++ material/models.py
@@ -2,15 +2,15 @@ from django.db import models
 # Create your models here.
 
 class Material(models.Model):
-    TIPO_CHOICES = {
-        "didático":"didático",
-        "paradidático":"paradidático",
-        "revista":"revista",
-        "DAL (S/Revisão)":"DAL (S/Revisão)",
-        "Outro":"Outro",
-    }
-    titulo = models.CharField(max_length = 255, null = True)
-    classificacao = models.CharField(max_length = 255, choices=TIPO_CHOICES)
+    TIPO_CHOICES = [
+        ("didático","didático"),
+        ("paradidático","paradidático"),
+        ("revista","revista"),
+        ("DAL (S/Revisão)","DAL (S/Revisão)"),
+        ("Outro","Outro"),
+    ]
+    classificacao = models.CharField(max_length = 255, null = True, choices = TIPO_CHOICES)
+    titulo = models.CharField(max_length = 255)
     acervo = models.CharField(max_length = 255)
     tiragem = models.CharField(max_length = 255)
     quantidade_paginas = models.PositiveIntegerField()
@@ -19,16 +19,17 @@ class Material(models.Model):
         return f"{self.titulo}, {self.classificacao}"
 
 class MaterialAdaptado(models.Model):    
-    MATERIAL_CHOICES = {
-        "braille":"braille",
-        "ampliado":"ampliado",
-    }
+    MATERIAL_CHOICES = [
+        ("braille","braille"),
+        ("ampliado","ampliado"),
+    ]
     material = models.ForeignKey(Material, on_delete=models.CASCADE)
     tipo = models.CharField(max_length = 255, choices = MATERIAL_CHOICES)
     quantidade_paginas = models.PositiveIntegerField()
     partes = models.PositiveIntegerField()
-    is_disponivel_para_assinatura = models.BooleanField(default=False, verbose_name="Disponível para assinatura?")
-    is_disponivel_para_pedido = models.BooleanField(default=False, verbose_name="Disponível para pedido?")
     arquivo = models.FileField(upload_to="arquivos/braille/%Y/%m", null = True, blank= True)
+    is_disponivel_para_pedido = models.BooleanField(default=False, verbose_name="Disponível para pedido?")
+    is_disponivel_para_assinatura = models.BooleanField(default=False, verbose_name="Disponível para assinatura?")
+    
     def __str__(self):
         return self.material.__str__() + ' | ' + self.tipo
