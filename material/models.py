069bfafa06037b4fbe7534d6e6600a52414d0854
michel diniz <micheldiniz@ibc.gov.br>
2024-03-25 11:42:29 -0300

    change file url construction based on tipo

--- material/models.py
+++ material/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+import os
+from datetime import datetime
 # Create your models here.
 
+def get_material_upload_path(instance, filename):
+    date_part = datetime.now().strftime('%Y/%m')
+
+    if isinstance(instance, MaterialAdaptado):
+        return os.path.join('materiais',instance.tipo, date_part,filename)
+    return os.path.join('materiais', date_part, filename)
 class Material(models.Model):
     class Meta:
         verbose_name_plural = 'Materiais'
@@ -21,7 +29,7 @@ class Material(models.Model):
     acervo = models.CharField(max_length = 255, null=True, blank=True)
     tiragem = models.CharField(max_length = 255, null=True, blank=True)
     quantidade_paginas = models.PositiveIntegerField(null=True, blank=True)
-    arquivo_original = models.FileField(upload_to="arquivos/%Y/%m", null = True, blank= True)
+    arquivo_original = models.FileField(upload_to=get_material_upload_path, null = True, blank= True)
     def __str__(self):
         return f'{self.titulo}, {self.classificacao}'
 
@@ -33,11 +41,12 @@ class MaterialAdaptado(models.Model):
         ("braille","braille"),
         ("ampliado","ampliado"),
     ]
+    
     material = models.ForeignKey(Material, on_delete=models.CASCADE)
     tipo = models.CharField(max_length = 255, choices = MATERIAL_CHOICES)
     quantidade_paginas = models.PositiveIntegerField(blank=True, null = True)
     partes = models.PositiveIntegerField(blank=True, null = True)
-    arquivo = models.FileField(upload_to="arquivos/braille/%Y/%m", null = True, blank= True)
+    arquivo = models.FileField(upload_to=get_material_upload_path, null = True, blank= True)
     is_disponivel_para_pedido = models.BooleanField(default=False, verbose_name="Disponível para pedido?")
     is_disponivel_para_assinatura = models.BooleanField(default=False, verbose_name="Disponível para assinatura?")
     
--- sisdib/urls.py
+++ sisdib/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
         auth_views.LoginView.as_view(template_name = 'login.html', redirect_authenticated_user = True),
         name = 'login'
     ),
-    path('logout/', auth_views.LogoutView.as_view(next_page = '/login/'), name = 'logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page = '/login/'), name = 'logout'),    
 ]
 
 if settings.DEBUG:
