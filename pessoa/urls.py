1e508b2f38c71bc5025665c4460c2b5972b7677e
michel diniz <micheldiniz@ibc.gov.br>
2024-04-12 17:06:29 -0300

    PessoaFisica list view page

--- pessoa/urls.py
+++ pessoa/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import success_page,pessoafisica_registration
+from .views import success_page,pessoafisica_registration,pessoa_all, deletar_pessoa
+
 
 urlpatterns = [
     path("cadastrar", pessoafisica_registration, name="pessoa_fisica_registration"),
-    path("success/", success_page, name="success_page"),
+    path("success", success_page, name="success_page"),
+    path("all", pessoa_all, name="pessoa-all"),
+    path("deletar/<int:id>", deletar_pessoa, name="deletar-pessoa"),
 ]
