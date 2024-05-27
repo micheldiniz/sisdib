521bc69c8f40b1b38985c5908b145695fca04b7e
michel diniz <micheldiniz@ibc.gov.br>
2024-05-27 16:39:38 -0300

    admin for new classes on pedidoAdaptacao

--- pedidoAdaptacao/admin.py
+++ pedidoAdaptacao/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from pedidoAdaptacao.models import Etapa,PedidoAdaptacao
+from pedidoAdaptacao.models import Etapa,PedidoAdaptacao, Avaliacao, Adaptacao, Diagramacao, Esteriotipia, Encadernacao, Revisao, Impressao, Transcricao
 # Register your models here.
 
 
@@ -10,10 +10,38 @@ class EtapaInline(admin.StackedInline):
     model = Etapa
     extra = 1
 
+class AdaptacaoInline(admin.StackedInline):
+    model = Adaptacao
+    extra = 0
+
+class DiagramacaoInline(admin.StackedInline):
+    model = Diagramacao
+    extra = 0
+
+class EsteriotipiaInline(admin.StackedInline):
+    model = Esteriotipia
+    extra = 0
+
+class EncadernacaoInline(admin.StackedInline):
+    model = Encadernacao
+    extra = 0
+
+class RevisaoInline(admin.StackedInline):
+    model = Revisao
+    extra = 0
+
+class ImpressaoInline(admin.StackedInline):
+    model = Impressao
+    extra = 0
+
+class TranscricaoInline(admin.StackedInline):
+    model = Transcricao
+    extra = 0
 
 class PedidoAdaptacaoAdmin(admin.ModelAdmin):
-    inlines = [EtapaInline]
-
+    inlines = [AdaptacaoInline,DiagramacaoInline,EsteriotipiaInline,EncadernacaoInline,ImpressaoInline,TranscricaoInline]
+    search_fields = ['solicitante']
+    autocomplete_fields = ['solicitante','material']
 
 admin.site.register(PedidoAdaptacao, PedidoAdaptacaoAdmin)
 admin.site.register(Etapa, EtapaAdmin)
