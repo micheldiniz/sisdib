d4246f9066a2258f0727ad2a8b2607de4d5a5cbf
michel diniz <micheldiniz@ibc.gov.br>
2024-06-06 14:35:32 -0300

    add model Revista to app assinatura

---
+++ assinatura/admin.py
@@ -0,0 +1,249 @@
+from django.contrib import admin
+from assinatura.models import Solicitante,Assinatura,Assinante,RegistroEnvioAssinaturas,EdicaoMaterialAssinatura, Remessa,Revista
+from pessoa.models import Endereco
+from django.db.models import Q
+from typing import Any
+from django.contrib import admin
+from django.db.models.fields.related import ForeignKey
+from django.forms.models import ModelChoiceField
+from django.http import HttpRequest, HttpResponse
+from django.db.models import Q
+from datetime import datetime
+from django.utils.safestring import mark_safe
+from django.shortcuts import render
+from typing import List
+from weasyprint import HTML
+
+@admin.action(description='cancelar assinatura(s)')
+def cancela_assinatura(modeladmin, request, queryset):
+    queryset.update(estado='cancelado', observacao='Cancelamento realizado em lote', data_ultima_alteracao = datetime.now())
+
+@admin.action(description='guia de correios')
+def guia_correio(modeladmin, request, queryset):
+    dict = {}
+        
+    for p in queryset:
+        materiais_adaptados  = []
+        edicoes = get_edicoes(p)
+        peso = 0
+        pacotes = 0
+        for e in edicoes:
+            materiais_adaptados.append(str(e.material.material.titulo))
+            peso += e.peso
+            pacotes += e.assinaturas.count()
+        remessas_qs = get_remessas(p)
+        remessas = {}
+        for remessa in remessas_qs:
+            remessas[remessa.id] = remessa
+
+        dict = {  'remessas' : remessas,
+                  'remetente': p.remetente,
+                  'identificacao': p.identificacao,
+                  'classificacao': ' + '.join(materiais_adaptados),
+                  'peso': peso,
+                  'pacotes':pacotes//2,
+                }
+
+    html_string = render(request, 'guia_correios_assinaturas.html', {'registroSaidaAssinaturas': dict }).content.decode('utf-8')
+
+    pdf = HTML(string=html_string).write_pdf()
+
+    response = HttpResponse(pdf, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="{0}.pdf"'.format('arquivo')
+
+    return response
+    # return render(request, 'guia_correios_assinaturas.html', {'registroSaidaAssinaturas': dict })
+
+
+# def get_quantidade_pacotes(ra: RegistroEnvioAssinaturas):
+#     edicoes = get_edicoes(ra)
+
+
+@admin.action(description='gerar etiquetas')
+def gerar_etiquetas(modeladmin, request, queryset):
+    dict = {}
+    
+    for p in queryset:        
+        edicoes = get_edicoes(p)   
+        solicitantes = []
+        for edicao in edicoes:
+            [solicitantes.append(assinatura.solicitante) for assinatura in edicao.assinaturas.all()]            
+        dict[p] = { 'solicitantes' : dict.fromkeys(solicitantes) }
+
+    return render(request, 'etiquetas_assinaturas.html', {
+        'registroSaidaAssinaturas': dict,
+    })
+
+def get_assinaturas(elem):    
+    assinaturas = []
+    for el in elem:
+        assinaturas = el.assinaturas.all()
+    return [assinatura for assinatura in assinaturas] 
+
+def get_edicoes(ra: RegistroEnvioAssinaturas) -> List['EdicaoMaterialAssinatura']:
+    edicoes_qs = EdicaoMaterialAssinatura.objects.filter(registro_envio = ra)
+    return [edicoes for edicoes in edicoes_qs]
+
+def get_remessas(ra: RegistroEnvioAssinaturas) -> List['Remessa']:
+    remessas_qs = Remessa.objects.filter(registro_envio = ra)
+    return [remessas for remessas in remessas_qs]
+
+def atualizar_assinaturas_no_registro_de_saida(modeladmin, request, queryset):
+    for p in queryset:        
+        edicoes = get_edicoes(p)
+        if p.enviado == False:
+            for edicao in edicoes:            
+                assinaturas = Assinatura.objects.filter(estado='vigente', material=edicao.material)            
+                edicao.quantidade_assinaturas_Es = quantidade_assinaturas_es(assinaturas)
+                edicao.quantidade_assinaturas_OE = quantidade_assinaturas_oe(assinaturas)
+                edicao.quantidade_assinaturas_RJ = quantidade_assinaturas_rj(assinaturas)
+                
+                print(edicao.quantidade_assinaturas_Es)
+                print(edicao.quantidade_assinaturas_OE)
+                print(edicao.quantidade_assinaturas_RJ)
+
+
+                edicao.assinaturas.set(assinaturas)
+                edicao.save()             
+
+def quantidade_assinaturas_es(ass):
+    total = 0
+    for a in ass:
+        if(a.solicitante.pessoa.is_estrangeiro == True):
+            # print(a.solicitante.pessoa)
+            total += 1
+    return total
+
+def quantidade_assinaturas_oe(ass):
+    total = 0
+    for a in ass:
+        if(get_UF_pessoa(a.solicitante) != 'RJ' and a.solicitante.pessoa.is_estrangeiro == False):
+            print(a.solicitante.pessoa)
+            total += 1
+    return total
+
+def quantidade_assinaturas_rj(ass):
+    total = 0
+    for a in ass:
+        if(get_UF_pessoa(a.solicitante) == 'RJ' and a.solicitante.pessoa.is_estrangeiro == False):
+            print(a.solicitante.pessoa)
+            total += 1
+
+    return total
+
+def get_UF_pessoa(solicitante:Solicitante):    
+    endereco = Endereco.objects.get(pessoa=solicitante.pessoa)
+    return endereco.estado
+
+class AssinanteInline(admin.StackedInline):
+    model = Assinante    
+    extra = 0
+
+class AssinaturaAdmin(admin.ModelAdmin):
+    # inlines = [AssinanteInline]
+    list_display = ['material','solicitante','data_registro', 'estado', 'observacao', 'data_ultima_alteracao']
+    list_editable = ['estado', 'observacao']
+    search_fields = ['material__material__titulo','estado','solicitante__pessoa__nome', 'material__tipo']    
+    autocomplete_fields = ['solicitante']
+    exclude = ('data_ultima_alteracao',)
+    actions = [cancela_assinatura]
+
+    def formfield_for_foreignkey(self, db_field: ForeignKey[Any], request: HttpRequest | None, **kwargs: Any) -> ModelChoiceField | None:
+
+        if db_field.name == 'material':
+            qs = super().formfield_for_foreignkey(db_field, request, **kwargs).queryset
+            object_id = request.resolver_match.kwargs.get('object_id')
+            
+            if (object_id):
+                assinatura_instance = Assinatura.objects.get(id=object_id)
+                current_material = assinatura_instance.material
+                filtered_qs = qs.filter(Q(is_disponivel_para_assinatura=True) | Q(material__materialadaptado = current_material)).distinct()
+                kwargs['queryset'] = filtered_qs
+
+                return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+            filtered_qs = qs.filter(Q(is_disponivel_para_assinatura=True)).distinct()
+            kwargs['queryset'] = filtered_qs
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+class SolicitanteAdmin(admin.ModelAdmin):
+    search_fields = ['pessoa__nome','pessoa__nacionalidade', 'pessoa__endereco__pais']
+    list_display = ['pessoa', 'assinaturas_ativas', 'assinaturas_canceladas']
+
+    def assinaturas_ativas(self, obj):
+        assinaturas = Assinatura.objects.filter(solicitante=obj, estado='vigente')
+        if assinaturas.count() < 1:
+            return 'nenhuma'
+        assinaturas_html = []
+        for assinatura in assinaturas:
+            html = '<a href="{0}">{1}</a></br>'.format(assinatura.get_admin_url(),str(assinatura.material))
+            assinaturas_html.append(html)
+        html_content = ''.join(assinaturas_html)
+        return mark_safe(html_content)
+
+    def assinaturas_canceladas(self, obj):
+        assinaturas = Assinatura.objects.filter(solicitante=obj, estado='cancelado')
+        if assinaturas.count() < 1:
+            return 'nenhuma'
+        assinaturas_html = []
+        for assinatura in assinaturas:
+            html = '<a href="{0}">{1}</a></br>'.format(assinatura.get_admin_url(),str(assinatura.material))
+            assinaturas_html.append(html)
+        html_content = ''.join(assinaturas_html)
+        return mark_safe(html_content)
+
+class EdicaoMaterialAssinaturaInline(admin.StackedInline):    
+    model = EdicaoMaterialAssinatura
+    extra = 0
+    readonly_fields = ['quantidade_assinaturas_RJ', 'quantidade_assinaturas_Es', 'quantidade_assinaturas_OE']
+    exclude = ['assinaturas']
+
+class RemessaInline(admin.StackedInline):
+    model = Remessa
+    extra = 0
+    exclude = ['observacao','ordem']
+
+class RegistroEnvioAssinaturasAdmin(admin.ModelAdmin):
+    search_fields = ['nome', 'assinaturas']
+    list_display = ['nome','enviado','data_registro', 'observacao','get_quantidade_assinaturas','get_quantidade_paginas']        
+    inlines = [EdicaoMaterialAssinaturaInline, RemessaInline]
+    actions = [gerar_etiquetas, guia_correio, atualizar_assinaturas_no_registro_de_saida]
+
+    # def save_related(self, request: Any, form: Any, formsets: Any, change: Any) -> None:
+    #     super().save_related(request, form, formsets, change)
+    
+    def get_quantidade_assinaturas(self,obj):
+        edicoes_relacionadas = EdicaoMaterialAssinatura.objects.filter(registro_envio = obj)
+        total = 0
+        for edicao in edicoes_relacionadas:
+            total += edicao.assinaturas.count()
+        return total
+
+    def get_quantidade_paginas(self,obj):
+        edicoes_relacionadas = EdicaoMaterialAssinatura.objects.filter(registro_envio = obj)        
+        total_paginas = 0
+        for edicao in edicoes_relacionadas:
+            qtd_paginas = edicao.quantidade_paginas * edicao.assinaturas.count()
+            total_paginas += qtd_paginas
+        return total_paginas
+
+    def get_assinaturas(self, obj):
+        return "\n".join([str(p) for p in obj.assinaturas.all()])
+    
+    def get_total_assinaturas(self, obj):
+        return obj.assinaturas.count()
+
+    get_assinaturas.short_description = 'Assinaturas enviadas'
+    get_total_assinaturas.short_description = 'Total de assinaturas enviadas'
+    get_quantidade_assinaturas.short_description = 'Total assinaturas'
+    get_quantidade_paginas.short_description = 'total paginas'
+
+class RevistaAdmin(admin.ModelAdmin):
+    pass
+
+
+
+admin.site.register(Revista, RevistaAdmin)
+admin.site.register(Solicitante, SolicitanteAdmin)
+admin.site.register(Assinatura, AssinaturaAdmin)
+admin.site.register(RegistroEnvioAssinaturas, RegistroEnvioAssinaturasAdmin)
---
+++ assinatura/models.py
@@ -0,0 +1,130 @@
+from django.db import models
+from pessoa.models import Pessoa
+from material.models import MaterialAdaptado
+from django.utils import timezone
+from datetime import datetime
+import os
+
+
+# Create your models here.
+
+def get_material_upload_path(instance, filename):
+    date_part = datetime.now().strftime('%Y/%m')
+
+    if isinstance(instance, MaterialAdaptado):
+        return os.path.join('materiais',instance.tipo, date_part,filename)
+    return os.path.join('materiais', date_part, filename)
+
+
+class Solicitante(models.Model):
+    data_registro = models.DateTimeField(auto_now_add=True)
+    pessoa = models.OneToOneField(Pessoa, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.pessoa.__str__()
+
+class Assinatura(models.Model):
+    ESTADO_CHOICES =[
+        ('vigente','vigente'),
+        ('cancelado','cancelado'),
+    ]
+    estado = models.CharField(max_length=255,choices=ESTADO_CHOICES, default='vigente')
+    material = models.ForeignKey(
+        MaterialAdaptado,
+        on_delete= models.CASCADE,
+        # limit_choices_to={'is_disponivel_para_assinatura':True}
+        )    
+    data_registro = models.DateTimeField(auto_now_add=True, verbose_name='Data de registro')
+    data_ultima_alteracao = models.DateTimeField(null=True)
+    observacao = models.CharField(max_length=255, verbose_name='Observação', blank=True)
+    solicitante = models.ForeignKey(Solicitante, on_delete=models.CASCADE, verbose_name='Solicitante / Assinante')
+
+    def __str__(self) -> str:
+        return f'{self.material}, solicitante: {self.solicitante}'
+    
+    def get_admin_url(self):
+        return '/admin/cliente/assinatura/{0}/change'.format(self.id)
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            self.data_ultima_alteracao = timezone.now()
+            # if self.your_field == 'some_value':
+            #     self.other_field = 'updated_value'
+        
+        super().save(*args, **kwargs)
+    
+class Assinante(models.Model):
+    nome = models.CharField(max_length=255, verbose_name="nome da pessoa que receberá o material")
+    assinatura = models.OneToOneField(Assinatura, on_delete=models.CASCADE)
+
+
+class RegistroEnvioAssinaturas(models.Model):    
+    class Meta:
+        verbose_name_plural = 'Configurar envio de Assinaturas'
+    nome = models.CharField(max_length=255, verbose_name='Nome', blank=False)
+    enviado = models.BooleanField(default=False, verbose_name='Enviado?')
+    remetente = models.CharField(max_length=255, verbose_name='Remetente',default='Instituto Benjamin Constant', blank=True)
+    identificacao = models.CharField(max_length=255, verbose_name='Identificação',default='Cecograma', blank=True)
+    data_registro = models.DateTimeField(verbose_name='Data de registro', auto_now_add=True)
+    observacao = models.CharField(max_length=255, verbose_name='Observação', blank=True)
+
+    def __str__(self) -> str:
+        return self.nome
+
+class TipoRemessa(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Remessa(models.Model):    
+    class Meta:
+        verbose_name = 'Remessa'
+        verbose_name_plural = 'Remessas para Envio (de edições de assinaturas)'
+    registro_envio = models.ForeignKey(RegistroEnvioAssinaturas, on_delete= models.CASCADE, null = True, blank= True)
+    tipo_remessa = models.ManyToManyField('TipoRemessa',max_length=255)
+    data_registro = models.DateTimeField(verbose_name='Data de registro', auto_now_add=True)
+    data_envio = models.DateField(null=True,blank=True)
+    quantidade = models.PositiveIntegerField(null=True, blank=True)
+    observacao = models.CharField(max_length=255, verbose_name='Observação', blank=True)
+    ordem = models.CharField(max_length=255, verbose_name='ordem', blank=True)
+
+    def __str__(self):        
+        tipo_remessas = [str(tipo_remessa) for tipo_remessa in self.tipo_remessa.all()]        
+        tipo_remessa = " e ".join(tipo_remessas)
+        return tipo_remessa + ' ' + self.ordem
+
+class Revista(models.Model):    
+    titulo = models.CharField(max_length = 255)
+    data_registro = models.DateTimeField(auto_now_add=True, verbose_name='Data de registro')
+
+    def __str__(self) -> str:
+        return self.titulo
+
+class EdicaoMaterialAssinatura(models.Model):
+    class Meta:
+        verbose_name = 'Edição da Assinatura'
+        verbose_name_plural = 'Edições da Assinatura'
+
+    material = models.ForeignKey(
+        MaterialAdaptado, 
+        on_delete= models.CASCADE,
+        limit_choices_to={'is_disponivel_para_assinatura':True},
+        null = True, blank= True
+        )
+    assinaturas = models.ManyToManyField(
+        Assinatura,
+        limit_choices_to={'estado':'vigente'}        
+        )
+    edicao = models.CharField(max_length = 255, null=True, blank=True)                
+    quantidade_paginas = models.PositiveIntegerField(null=True, blank=True, default=0)
+    peso = models.PositiveIntegerField(null=True, blank=True, default=0)
+    registro_envio = models.ForeignKey(RegistroEnvioAssinaturas, on_delete= models.CASCADE, null = True, blank= True)
+    arquivo_original = models.FileField(upload_to=get_material_upload_path, null = True, blank= True)
+    quantidade_assinaturas_RJ = models.PositiveIntegerField(null=True,verbose_name='Qtd. ass. Rio de Janeiro (ativas)', blank=True, default=0)
+    quantidade_assinaturas_Es = models.PositiveIntegerField(null=True,verbose_name='Qtd. ass. Estrangeiros (ativas)', blank=True, default=0)
+    quantidade_assinaturas_OE = models.PositiveIntegerField(null=True,verbose_name='Qtd. ass. Outros Estados (ativas)', blank=True, default=0)
+
+    def __str__(self) -> str:
+        return '{0}. Edição: {1}'.format(self.material.material, self.edicao)
+    
