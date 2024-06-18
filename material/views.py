3f9ded645c5c8950f35f360b4fd1f392f555f628
michel diniz <micheldiniz@ibc.gov.br>
2024-06-18 17:14:23 -0300

    view methods for MaterialAdaptado

--- material/views.py
+++ material/views.py
@@ -1,42 +1,69 @@
 from django.shortcuts import render
-from .forms import MaterialAdaptadoFormSet, MaterialForm
+from .forms import MaterialAdaptadoFormSet, MaterialForm, MaterialAdaptadoForm
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from material.models import Material,MaterialAdaptado
+from django.urls import reverse
+from django.shortcuts import redirect
 
 def index(request):
     return HttpResponse("Index Material!")
 
 def cadastrar_material(request):
-    if request.method == 'POST':
-        materialAdaptado_form = MaterialAdaptadoFormSet(request.POST, request.FILES)
+    if request.method == 'POST':        
         material_form = MaterialForm(request.POST, request.FILES)
-        
-        # print(material_form)
-        # material = material_form.save()
-        
-        # if materialAdaptado_form.is_valid() and material_form.is_valid():
-        #     material = material_form.save()
-        #     print("#################################")
-        #     print(material)
-        #     materialAdaptado_form.instance = material
-        #     materialAdaptado_form.save()            
-        #     return render(request, 'success.html')
+  
         if material_form.is_valid():
             material = material_form.save()               
-            return render(request, 'success.html')
+            return redirect(reverse(list_materials))
     else:
-        material_form = MaterialForm()
-        # materialAdaptado_form = MaterialAdaptadoFormSet()
+        material_form = MaterialForm()        
+
     return render(request, 'material/cadastrar.html', {
-        'material_form' : material_form,
-        # 'materialAdaptado_form' : materialAdaptado_form,
+        'form' : material_form,
+        'titulo':"Material",
+        'subtitulo':"Original",
+        'app':'material',
+        'app_description':'Material',
     })
 
 def list_materials(request):
-  all_materials = Material.objects.all()
-  context = {'materiais': all_materials}
+    all_materials = Material.objects.all()
+
+    field_names = [field.name for field in Material._meta.fields]
+
+    context = {'objects': all_materials,
+            'titulo':"Material",
+            'subtitulo':"Original",
+            'fields':field_names,
+            }
+    return render(request, 'material/lista.html', context)
+
+def cadastrar_material_adaptado(request):
+    if request.method == 'POST':
+        materialAdaptado_form = MaterialAdaptadoForm(request.POST, request.FILES)        
+  
+        if materialAdaptado_form.is_valid():
+            material_adaptado = materialAdaptado_form.save()               
+            return redirect(reverse(list_materiais_adaptados))
+    else:
+        materialAdaptado_form = MaterialAdaptadoForm()
+
+    return render(request, 'material/cadastrar.html', {
+        'form' : materialAdaptado_form,
+        'titulo':"Material",
+        'subtitulo':"Adaptado",
+        'app':'material/adaptado',
+    })
+
+def list_materiais_adaptados(request):
+  all_materiais_adaptados = MaterialAdaptado.objects.all()
+  context = {'objects': all_materiais_adaptados,
+            'titulo':"Material",
+            'subtitulo':"Adaptado",
+            'app_description':'Material Adaptado'
+            }
   return render(request, 'material/lista.html', context)
 
 def visualizar_material(request):
