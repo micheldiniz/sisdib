e89c70c6ad16de40234950cfa27f8b00b483eff2
michel diniz <micheldiniz@ibc.gov.br>
2024-06-18 17:13:38 -0300

    remove unnecessary code overwritten

--- material/forms.py
+++ material/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from material.models import MaterialAdaptado, Material
 from django.forms.models import inlineformset_factory
+from django_select2.forms import Select2Widget
 
 
 class MaterialForm(forms.ModelForm):
@@ -55,10 +56,10 @@ class MaterialAdaptadoForm(forms.ModelForm):
 
     class Meta:
         model = MaterialAdaptado
-        fields = ['tipo', 'quantidade_paginas', 'partes','tamanho','is_disponivel_para_pedido','arquivo']
-        exclude = ['Id']
-        labels = {          
-            'tipo':'Tipo',            
+        fields = ['material','tipo', 'quantidade_paginas', 'partes', 'tamanho','is_disponivel_para_pedido','arquivo']        
+        labels = {
+            'material':'material',
+            'tipo':'Tipo',
             'quantidade_paginas':'Quantidade de Páginas',
             'tamanho':'Tamanho',
             'partes':'Partes',
@@ -66,25 +67,15 @@ class MaterialAdaptadoForm(forms.ModelForm):
             'arquivo':'Arquivo adaptado',
         }
         widgets = {
+            'material': forms.Select(attrs={'class':'form-select'}),
             'tipo': forms.Select(attrs={'class': 'form-select'}),
             'quantidade_paginas': forms.TextInput(attrs={'class': 'form-control'}),
             'partes': forms.TextInput(attrs={'class': 'form-control'}),
             'tamanho': forms.TextInput(attrs={'class': 'form-control'}),
             'is_disponivel_para_pedido': forms.CheckboxInput(attrs={'class': 'form-control'}),
             'arquivo': forms.FileInput(attrs={'class': 'form-control'}),
+            # 'material': Select2Widget,
         }
 
-    def __init(self, *args,**kwargs):
-        super(MaterialAdaptadoForm, self).__init__(*args, **kwargs)
-        self.material_formset = MaterialAdaptadoFormSet(instance=self.instance)
-
-    def is_valid(self):
-        return super(MaterialAdaptadoForm, self).is_valid() and self.material_formset.is_valid()
-
-    def save(self, commit = True):
-        instance = super(MaterialAdaptadoForm, self).save(commit=commit)
-        self.material_formset.instance = instance
-        self.material_formset.save(commit=commit)
-        return instance
 
 MaterialAdaptadoFormSet = inlineformset_factory(Material, MaterialAdaptado, form=MaterialAdaptadoForm, extra=1, can_delete=True)
