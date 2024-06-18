1a9c398e574c0036753ff95642deda667d52bcfa
michel diniz <micheldiniz@ibc.gov.br>
2024-06-18 17:13:51 -0300

    new paths for Material Adaptado

--- material/urls.py
+++ material/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("cadastrar", views.cadastrar_material, name="cadastrar_material"),
     path("visualizar", views.visualizar_material, name="visualizar_material"),
     path("todos", views.list_materials, name="list_materials"),
+    path("adaptado/cadastrar", views.cadastrar_material_adaptado, name="cadastrar_material_adaptado"),
+    # path("adaptado/visualizar", views., name="visualizar_material"),
+    path("adaptado/todos", views.list_materiais_adaptados, name="list_materials_adaptados"),
     path('material/<int:material_id>/download', views.download_file, name='download_file')
 ]
